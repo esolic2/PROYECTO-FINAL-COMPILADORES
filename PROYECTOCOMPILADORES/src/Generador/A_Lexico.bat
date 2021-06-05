60a41d75d1e98521ab280c39dd1840364307b35a
esolic2 <85380699+esolic2@users.noreply.github.com>
2021-06-05 01:42:34 -0600

    Add files via upload

---
+++ PROYECTOCOMPILADORES/src/Generador/A_Lexico.bat
@@ -0,0 +1,6 @@
+SET JAVA_HOME= "C:\Program Files\Java\jdk1.8.0_201\bin"
+SET PATH=%JAVA_HOME%;%PATH%
+SET CLASSPATH=%JAVA_HOME%;
+cd C:\Users\SC\Documents\NetBeansProjects\PROYECTOCOMPILADORES\src\proyectocompiladores
+
+pause
